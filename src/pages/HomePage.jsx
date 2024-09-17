5e7958d8a26474aae5f86894e104c621d37e9585
Ohad Taizi <74296478+ohadtaizi@users.noreply.github.com>
2024-09-17 11:35:36 +0300

    change home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,8 +2,8 @@
 export function HomePage() {
     return (
         <section>
-            <h1>Home sweet Home</h1>
-            <span> commiting the system! </span>
+     
+            <p> ohad</p>
         </section >
     )
 }
