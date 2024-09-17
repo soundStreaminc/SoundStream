a8119bee23af4f23143066409ae71a4312c467ed
aviavi16 <eranis54321@gmail.com>
2024-09-17 11:16:29 +0300

    avinoam commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,8 @@ export function HomePage() {
         <section>
             <h1>Home sweet Home</h1>
             <span> commiting the system! </span>
+            <span> commiting the system2! </span>
+
         </section >
     )
 }
