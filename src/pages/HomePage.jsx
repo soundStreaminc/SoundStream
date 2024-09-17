50de6d19101cd9f115414ddeae2e0dc8c31128fb
Ohad Taizi <74296478+ohadtaizi@users.noreply.github.com>
2024-09-17 12:08:05 +0300

    home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,12 @@
 
 export function HomePage() {
+    function hello(){
+        console.log("hello word");
+        
+    }
     return (
         <section>
-     
+     {hello()}
             <p> ohad</p>
         </section >
     )
