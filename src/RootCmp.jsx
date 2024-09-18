b2e2876729e599cebed499fba774130b22567074
aviavi16 <eranis54321@gmail.com>
2024-09-18 09:14:42 +0300

    fixing the root

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -4,19 +4,20 @@ import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import { StationDetails } from './pages/StationDetails'
 
-//import { AppHeader } from '../cmps/AppHeader'
+import { AppHeader } from './cmps/AppHeader'
+import { AppFooter } from './cmps/AppFooter'
 
 export function RootCmp() {
     return (
         <div>
-            {/* <AppHeader /> */}
+            <AppHeader />
             <main>
                 <Routes>
                     <Route path="" element={<HomePage />} />
                     <Route path="/:stationId" element={<StationDetails />} />
                 </Routes>
             </main>
-           
+            <AppFooter />
         </div>
     )
 }
