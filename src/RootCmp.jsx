faf3575b6897ef4fe064a7c32d1bb0705b1b484e
Ohad Taizi <74296478+ohadtaizi@users.noreply.github.com>
2024-09-28 19:00:51 +0300

    merge rootcmp

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,33 +1,3 @@
-import React from 'react'
-import { Routes, Route } from 'react-router'
-
-import { HomePage } from './pages/HomePage'
-import { StationDetails } from './pages/StationDetails'
-
-import { AppHeader } from './cmps/AppHeader'
-import { AppFooter } from './cmps/AppFooter'
-import { SideBar } from './cmps/SideBar'
-
-export function RootCmp() {
-    return (
-        <div className='app-container'>
-
-                <AppHeader />
-                <div className="main-content-container">
-                <SideBar/>
-                <main>
-                    <Routes>
-                        <Route path="" element={<HomePage />} />
-                        <Route path="/:stationId" element={<StationDetails />} />
-                    </Routes>
-                </main>
-                </div>
-                <AppFooter />
-
-            
-        </div>
-    )
-}
 
 
 
@@ -40,20 +10,22 @@ import { StationDetails } from './pages/StationDetails'
 import { AppHeader } from './cmps/AppHeader'
 import { AppFooter } from './cmps/AppFooter'
 import { RightSidebar } from './pages/RightSidebar'
-import { LeftSidebar } from './pages/LeftSidebar'
-
+// import { LeftSidebar } from './pages/LeftSidebar'
+import { SideBar } from './cmps/SideBar'
 export function RootCmp() {
 
     return (
         <div className='main-app'>
                 <AppHeader />
-                <LeftSidebar />        
+                <div className="main-content-container">
+                <SideBar/> 
                 <main className='container'>
                     <Routes>
                             <Route path="" element={<HomePage />} />
                             <Route path="/:stationId" element={<StationDetails />} />
                     </Routes>
                 </main>
+                </div>
                 <RightSidebar />
                 <AppFooter /> 
         </div>
