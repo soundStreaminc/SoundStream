77c947dc1920d4333535e143f49e9ab0abf0b0ad
Avinoam <eranis54321@gmail.com>
2024-12-08 23:09:01 +0200

    added the poiner cursor to mouse, added some more css fixes.

--- src/cmps/trackPreviewCmps/TrackPreview_Album.jsx
+++ src/cmps/trackPreviewCmps/TrackPreview_Album.jsx
@@ -1,9 +1,18 @@
+import { useNavigate } from "react-router";
+
 export function TrackPreview_Album({track, tracksDisplayType}){
     const isPlaylist = tracksDisplayType === 'playlist'
+    const navigate = useNavigate()
 
+    function onButtonClickHandler (  ) {
+        navigate(`/album/${track.album?.id}`);
+    }
+    
     return (
         <div className="track-preview-album">
-            {isPlaylist && <div className="track-album">{track?.album?.name}</div>}
+            {isPlaylist && <div className="track-album" onClick={onButtonClickHandler}>
+                {track?.album?.name}
+            </div>}
         </div>   
     )
 }
