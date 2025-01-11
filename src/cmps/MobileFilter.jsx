d7f38c38aab77b78fba46e23e2d01bb0411ede6f
Avinoam <eranis54321@gmail.com>
2025-01-11 20:44:43 +0200

    some fixes for the mobile headers.

--- src/cmps/MobileFilter.jsx
+++ src/cmps/MobileFilter.jsx
@@ -1,6 +1,21 @@
+import { useState } from 'react';
 import UserIcon from '../assets/svgs/noImageArtist.svg?react';
 
 export function MobileFilter(){
+    const [activeButton, setActiveButton] = useState('all'); // Track which button is active
+
+    const handleAllClick = () => {
+        setActiveButton('all'); // Set All as the active button
+    }
+
+    const handleMusicClick = () => {
+        setActiveButton('music'); // Set Music as the active button
+    }
+
+    const handlePodcastsClick = () => {
+        setActiveButton('podcasts'); // Set Podcasts as the active button
+    }
+
     return(
         <section className="mobile-filter-container">
             <button className="mobile-user-profile-btn" >
@@ -11,10 +26,25 @@ export function MobileFilter(){
                 </figure>
             </button>
             <div className="mobile-filters-background">
-                <div className="content-filters"  onClick={()=> getDateByArtist( inputArtist.current )}>
-                    <button>All</button>
-                    <button>Music</button>
-                    <button>Podcasts</button>
+                <div className="content-filters">
+                    <button className={`app-header-filters icon ${activeButton === 'all' ? 'active' : ''}`}
+                        onClick={() => {
+                            handleAllClick()
+                        }}>
+                            All
+                    </button>
+                    <button className={`app-header-filters icon ${activeButton === 'music' ? 'active' : ''}`}
+                        onClick={() => {
+                            handleMusicClick()
+                        }}>
+                            Music
+                    </button>
+                    <button  className={`app-header-filters icon ${activeButton === 'podcasts' ? 'active' : ''}`}
+                        onClick={() => {
+                            handlePodcastsClick()
+                        }}>
+                            Podcasts
+                    </button>
                 </div>
             </div>
         </section>
